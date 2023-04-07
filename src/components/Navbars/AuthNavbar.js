43ac34b463b9e1fd221b543b23b5d50391bd2328
Devashish <devashishjaybhaye@gmail.com>
2023-04-07 15:13:15 +0530

    Changes

--- src/components/Navbars/AuthNavbar.js
+++ src/components/Navbars/AuthNavbar.js
@@ -106,7 +106,7 @@ export default function Navbar(props) {
               <span class="sr-only">Open main menu</span>
               <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
             </button>
-            <div class=" w-full md:block md:w-auto inline-flex" id="navbar-solid-bg">
+            <div class=" w-full md:block md:w-auto inline-flex"  id="navbar-solid-bg">
               <ul class="inline-flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700 align-middle ">
                 <li>
                   <a href="#" class="block py-3 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</a>
