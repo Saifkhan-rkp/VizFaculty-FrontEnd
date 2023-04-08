0af87e4170983b293c06e62c7794b9de2e13ff46
unknown <hardikthakre17@gmail.com>
2023-04-08 10:49:49 +0530

    Salary Page added

--- src/layouts/facultyLayout/Salary.js
+++ src/layouts/facultyLayout/Salary.js
@@ -3,39 +3,32 @@ import React from "react";
 export default function Salary() {
   return (
     <>
-      <div className="relative overflow-x-auto">
-        <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <a href="/">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Salary Status
-            </h5>
-          </a>
-          <p className="mb-3 font-normal text-gray-900 dark:text-white">
-            The salary below is salary of current month so for ..
-          </p>
-          <h6 className="mb-2 py-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            7800 Rs
-          </h6>
-          <button
-            type="button"
-            class="px-8 py-3 text-white bg-blue-300 rounded focus:outline-none"
-            disabled
-          >
-            Process Salary
-            <svg
-              aria-hidden="true"
-              className="w-4 h-4 ml-2 -mr-1"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </button>
+      <div className="relative overflow-x-auto bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <div className="px-4 md:px-10 py-4 md:py-7">
+          <div className="flex flex-wrap items-center">
+            <div className="relative w-full max-w-full flex-grow flex-1">
+              <h6 className="py-2 uppercase text-slate-400 mb-1 text-xs font-semibold">
+                Salary Status
+              </h6>
+            </div>
+          </div>
+          <h2 className="py-4 font-medium text-blue-700 dark:text-white">
+            Check your current month salary here and keep track of it !!
+          </h2>
+          <div className="flex justify-between mb-1">
+            <span className="text-base font-medium text-blue-700 dark:text-white">
+              Salary Tracker
+            </span>
+            <span className="text-sm font-medium text-blue-700 dark:text-white">
+              1300 Rs
+            </span>
+          </div>
+          <div className="w-full bg-white-200 rounded-full h-2.5 dark:bg-white-700">
+            <div
+              className="bg-blue-600 h-2.5 rounded-full"
+              style={{ width: "15%" }}
+            ></div>
+          </div>
         </div>
       </div>
     </>
