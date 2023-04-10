ffd312407ab03fa3d75b6efe440b9e12d83ea16d
unknown <hardikthakre17@gmail.com>
2023-04-10 21:57:39 +0530

    Components Created

--- src/layouts/facultyLayout/FacultyDashboard.jsx
+++ src/layouts/facultyLayout/FacultyDashboard.jsx
@@ -6,16 +6,26 @@ import FooterAdmin from "../../components/Footers/FooterAdmin";
 
 import { Outlet } from "react-router-dom";
 
+const links = [
+  { heading: "My Dashboard", link: "/faculty", icon: "fas fa-tv" },
+  { heading: "Settings", link: "/faculty/settings", icon: "fas fa-tools" },
+  {
+    heading: "SALARY STATUS",
+    link: "/faculty/salaryStatus",
+    icon: "fas fa-table",
+  },
+  {
+    heading: "ATTENDANCE",
+    link: "/faculty/attendance",
+    icon: "fas fa-map-marked",
+  },
+  // {heading:"Settings", link:"/adminDept/settings", icon:""},
+];
+
 export default function FacultyDashboard() {
   return (
     <>
-      <Sidebar
-        heading="Faculty Layout Pages"
-        item1="My Dashboard"
-        item2="Settings"
-        item3="Attendence"
-        item4="Salary Status"
-      />
+      <Sidebar heading="Quick Accessibility" linksAndHeadings={links} />
       <div className="relative md:ml-64 bg-slate-100">
         <AdminNavBar />
         <HeaderStats userType="faculty" />
