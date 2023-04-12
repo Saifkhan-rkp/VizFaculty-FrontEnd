c013563bbfafd30cf68f7d87c8006a3e2e6ac506
unknown <hardikthakre17@gmail.com>
2023-04-13 00:44:37 +0530

    Card Settings Changes

--- src/components/Cards/CardSettings.js
+++ src/components/Cards/CardSettings.js
@@ -109,12 +109,13 @@ export default function CardSettings() {
                     className="block uppercase text-slate-600 text-xs font-bold mb-2"
                     htmlFor="grid-password"
                   >
-                    Authentication Code
+                    Authorization Of
                   </label>
                   <input
+                    Disabled
                     type="text"
                     className="border-0 px-3 py-3 placeholder-slate-300 text-slate-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue=""
+                    defaultValue="Can Edit Time Table"
                   />
                 </div>
               </div>
