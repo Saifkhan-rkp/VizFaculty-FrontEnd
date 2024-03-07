bff5ce2c5b393916a1c030f5da8fd09ecceb0a62
sssplCodeStar <ssspl.official@gmail.com>
2024-03-08 01:04:50 +0530

    attendance form changes & CardAttendance integration done

--- src/components/Cards/CardAttendence.js
+++ src/components/Cards/CardAttendence.js
@@ -1,12 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
+import { getAuthData } from "../../utils/utils";
 
 export default function CardAttendence(props) {
+  const auth = getAuthData();
+  const { data: attendanceData, isLoading, isError, refetch } = useQuery(["attendanceData"], () => axios.get(`${process.env.REACT_APP_API_KEY}/api/attendance/by-month/${(new Date().getMonth() + 1)}`,
+    {
+      headers: {
+        authorization: `Bearer ${auth?.accessToken}`
+      }
+    })
+    .then(res => res.data));
+
+  // console.log(attendanceData)
   return (
     <>
       <div className="relative overflow-x-auto bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
         <div className="px-4 md:px-10 py-4 md:py-7">
           <div className="flex items-center justify-between">
-            <div className="text-white bg-gray-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800  dark:border-gray-700py-3 px-4 flex items-center text-sm font-medium leading-none text-gray-600 bg-gray-200 cursor-pointer rounded">
+            <div className="text-white bg-gray-800 rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800  dark:border-gray-700py-3 px-4 flex items-center font-medium leading-none cursor-pointer ">
               <p>Filter By:</p>
               <select className="focus:outline-none bg-transparent ml-1">
                 <option className="text-sm text-indigo-800">
@@ -65,24 +78,27 @@ export default function CardAttendence(props) {
             </tr>
           </thead>
           <tbody>
-            {props.Attendencedata.map((data) => (
-              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th
-                  scope="row"
-                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  {data.date}
-                </th>
-                <td class="px-6 py-4">{data.day}</td>
-                <td class="px-6 py-4">{data.subject}</td>
-                <td class="px-6 py-4">{data.yearAndBranch}</td>
-                <td class="px-6 py-4">{data.teachingType}</td>
-                <td class="px-6 py-4">{data.timeFrom}</td>
-                <td class="px-6 py-4">{data.timeTo}</td>
-                <td class="px-6 py-4">{data.totalHours}</td>
-                <td class="px-6 py-4">{data.rate}</td>
-                <td class="px-6 py-4">{data.amount}</td>
-              </tr>
+            {!isLoading && attendanceData?.data.length===0 && <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"> No Attendence Data found</tr>}
+            {!isLoading && attendanceData?.data.map((data) => (
+              data.attendance.map(att => (
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={att?._id}>
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {new Date(data.date).toDateString()}
+                  </th>
+                  <td class="px-6 py-4">{data.day}</td>
+                  <td class="px-6 py-4">{att.subject}</td>
+                  <td class="px-6 py-4">{att.yearAndBranch}</td>
+                  <td class="px-6 py-4">{att.teachingType}</td>
+                  <td class="px-6 py-4">{att.timeFrom}</td>
+                  <td class="px-6 py-4">{att.timeTo}</td>
+                  <td class="px-6 py-4">{att.totalHours}</td>
+                  <td class="px-6 py-4">{att.rate}</td>
+                  <td class="px-6 py-4">{att.amount}</td>
+                </tr>
+              ))
             ))}
           </tbody>
         </table>
