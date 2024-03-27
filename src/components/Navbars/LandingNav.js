7890bad2c3236dcb3f67088d43b46eccf92ab8b8
sssplCodeStar <ssspl.official@gmail.com>
2024-03-28 02:10:31 +0530

    Landing changed

--- src/components/Navbars/LandingNav.js
+++ src/components/Navbars/LandingNav.js
@@ -119,7 +119,7 @@ export default function LandingNav() {
                                     <Link
                                         to={"/auth/register"}
                                         title="Sign Up"
-                                        className=" text-center justify-center text-sky-900 bg-gray-300 hover:bg-gray-400 focus:ring-4 focus:ring-sky-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 focus:outline-none lg:visible">
+                                        className="hidden lg:block text-center justify-center text-sky-900 bg-gray-300 hover:bg-gray-400 focus:ring-4 focus:ring-sky-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 focus:outline-none lg:visible">
                                         Sign Up
                                     </Link>
                                     <Link
@@ -148,7 +148,7 @@ export default function LandingNav() {
                         <div className={`items-center justify-between w-full lg:flex lg:w-auto lg:order-1 ${show ? "" : "hidden"}`} id="mobile-menu-2">
                             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                                 <li>
-                                    <a href="#"
+                                    <a href="/"
                                         className="block py-2 pl-3 pr-4 text-white bg-sky-500 rounded lg:bg-transparent lg:text-sky-500 lg:p-0"
                                         aria-current="page">Home</a>
                                 </li>
@@ -170,18 +170,19 @@ export default function LandingNav() {
                                         Features
                                     </a>
                                 </li>
-                                {/* <li>
-                                    <a href="#"
-                                        className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-sky-500 lg:p-0">
-                                        Team
-                                    </a>
-                                </li> */}
                                 <li>
                                     <a href="#contact-us"
                                         className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-sky-500 lg:p-0">
                                         Contact
                                     </a>
                                 </li>
+                                {!auth &&
+                                    <li>
+                                        <a href="/auth/register"
+                                            className="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-sky-500 lg:p-0 sm:hidden">
+                                            Sign up
+                                        </a>
+                                    </li>}
                             </ul>
                         </div>
                     </div>
